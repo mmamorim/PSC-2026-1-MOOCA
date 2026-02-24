83613bb2d54797754f07336f0b715b6e4a13a726
Marcelo <marc.m.amorim@gmail.com>
2026-02-24 11:45:04 -0300

    v1

---
+++ Projeto02/Teste.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Teste {
+
+    public static void main(String args[]) {
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe uma idade:");
+        int n = leitor.nextInt();
+        System.out.println("Valor da idade digitada: " + n);
+
+    }
+}

cd95372751af24e658b40e7e26dacc18623f182d
Marcelo <marc.m.amorim@gmail.com>
2026-02-24 11:04:51 -0300

    v1

--- Projeto02/Prog.java
+++ Projeto02/Prog.java
@@ -5,6 +5,9 @@ public class Prog {
     public static void main(String args[]) {
          
         System.out.println("oi gente");
+        System.out.println("oi gente");
+        System.out.println("oi gente");
+        System.out.println("oi gente");
     }
 
 }

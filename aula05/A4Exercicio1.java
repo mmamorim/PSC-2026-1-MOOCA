e24d37d0ee1942f7bbee5fc3dc20d5936c06def0
Marcelo <marc.m.amorim@gmail.com>
2026-03-24 11:36:57 -0300

    v1

--- aula05/A4Exercicio1.java
+++ aula05/A4Exercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercicio1 {
+public class A4Exercicio1 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         String texto;
---
+++ aula05/A4Exercicio2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class A4Exercicio2 {
+    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite um texto:");
+        String texto = leitor.nextLine();
+        System.out.println("Você digitou: "+texto);
+        texto = texto.toLowerCase();
+        System.out.println("Como ficou: "+texto);
+        int qtdeVogais = 0;
+        String letrasVogais = "aáàaãaâeéèiou";
+
+        for(int i=0; i < texto.length(); i++) {
+            char letra = texto.charAt(i);
+            if(letrasVogais.contains(""+letra)) {
+                qtdeVogais++;
+            }
+            // if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+            //     qtdeVogais++;
+            // }
+        }
+        System.out.println("Qtde vogais: "+qtdeVogais);
+    }
+}

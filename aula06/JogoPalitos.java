89f093750b5a26de25d8e28581303b31aa361dee
Marcelo <marc.m.amorim@gmail.com>
2026-03-31 11:12:50 -0300

    v1

--- aula06/JogoPalitos.java
+++ aula06/JogoPalitos.java
@@ -1,13 +1,15 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class JogoPalitos {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("+---------------------+");
         System.out.println("| Jogo dos palitinhos |");
         System.out.println("+---------------------+");
 
+        int rodada = 0;
         int qtdePJ = 3;
         int qtdePC = 3;
         int qtdePJEscolhido = 0;
@@ -15,26 +17,46 @@ public class JogoPalitos {
         int qtdePJPalpite = 0;
         int qtdePCPalpite = 0;
 
-        System.out.println("Digite quantos palitos quer separar:");
-        qtdePJEscolhido = leitor.nextInt();
-        System.out.println("Digite qual seu paltite:");
-        qtdePJPalpite = leitor.nextInt();
-        qtdePCEscolhido = (int)(Math.random() * 10) % (qtdePC+1);
-        qtdePCPalpite = qtdePCEscolhido +  (int)(Math.random() * 10) % (qtdePJ+1);
+        while (qtdePJ > 0 && qtdePC > 0) {
+            rodada++;
+            System.out.println("********************");
+            System.out.println("RODADA "+rodada);
+            System.out.println("Vc tem "+qtdePJ+" palitos no total");
+            System.out.println("Computador tem "+qtdePC+" palitos no total");
+            System.out.println("********************");
 
-        System.out.println("qtdePJEscolhido: "+qtdePJEscolhido);
-        System.out.println("qtdePCEscolhido: "+qtdePCEscolhido);
-        System.out.println("qtdePJPalpite: "+qtdePJPalpite);
-        System.out.println("qtdePCPalpite: "+qtdePCPalpite);
+            System.out.println("Digite quantos palitos quer separar:");
+            qtdePJEscolhido = leitor.nextInt();
+            System.out.println("Digite qual seu paltite:");
+            qtdePJPalpite = leitor.nextInt();
+            qtdePCEscolhido = (int) (Math.random() * 10) % (qtdePC + 1);
+            qtdePCPalpite = qtdePCEscolhido + (int) (Math.random() * 10) % (qtdePJ + 1);
 
-        if() {
-            System.out.println("Você acertou");
-        } else {
-            if() {
-                System.out.println("Computador acertou");
+            System.out.println("qtdePJEscolhido: " + qtdePJEscolhido);
+            System.out.println("qtdePCEscolhido: " + qtdePCEscolhido);
+            System.out.println("qtdePJPalpite: " + qtdePJPalpite);
+            System.out.println("qtdePCPalpite: " + qtdePCPalpite);
+
+            if (qtdePJPalpite == qtdePCEscolhido + qtdePJEscolhido) {
+                System.out.println("Você acertou");
+                qtdePJ--;
             } else {
-                System.out.println("Ninguém acertou");
+                if (qtdePCPalpite == qtdePCEscolhido + qtdePJEscolhido) {
+                    System.out.println("Computador acertou");
+                    qtdePC--;
+                } else {
+                    System.out.println("Ninguém acertou");
+                }
             }
+
         }
+
+        if(qtdePJ == 0) {
+            System.out.println("Vc GANHOU!");
+        } else {
+            System.out.println("COMPUTADOR GANHOU!");
+        }
+        System.out.println("GAME OVER");
+        
     }
 }

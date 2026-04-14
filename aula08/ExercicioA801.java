94bc010f7575c92a3613f0a560266e98780ed75e
Marcelo <marc.m.amorim@gmail.com>
2026-04-14 10:44:30 -0300

    v1

--- aula08/ExercicioA801.java
+++ aula08/ExercicioA801.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class ExercicioA801 {
+    static String[] produtos = { "Arroz", "Feijão", "Macarrão", "Leite", "Café" };
+    static double[] precos = { 5.50, 7.20, 4.00, 3.80, 8.90 };
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        String[] produtos = { "Arroz", "Feijão", "Macarrão", "Leite", "Café" };
-        double[] precos = { 5.50, 7.20, 4.00, 3.80, 8.90 };
+
         int codigo = 0;
         int qtde = 0;
         String textoLista = "";
@@ -13,15 +14,15 @@ public class ExercicioA801 {
         while (codigo >= 0) {
 
             imprimeCabecalho();
-            for (int i = 0; i < produtos.length; i++) {
-                System.out.println("" + i + " - " + produtos[i] + " (R$ " + precos[i] + ")");
-            }
-            System.out.println("---------------------------");
-            System.out.println("Digite a opção (-1 p/ SAIR): ");
-            codigo = leitor.nextInt();
+            imprimeOpcoesMenu();
+            codigo = lerOpcao();
+            
+
             if (codigo >= 0) {
-                System.out.println("Digite a Qtde: ");
-                qtde = leitor.nextInt();
+                
+                //System.out.println("Digite a Qtde: ");
+                //qtde = leitor.nextInt();
+                qtde = lerQtde();
 
                 textoLista = textoLista + "[" + codigo + "] " + produtos[codigo];
                 textoLista = textoLista + " " + qtde + " unidades";
@@ -38,5 +39,19 @@ public class ExercicioA801 {
         System.out.println("---------------------------");
         System.out.println("MENU DE PRODUTOS");
         System.out.println("---------------------------");
+    }
+
+    public static void imprimeOpcoesMenu() {
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.println("" + i + " - " + produtos[i] + " (R$ " + precos[i] + ")");
+        }
+    }
+
+    public static int lerOpcao() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("---------------------------");
+        System.out.println("Digite a opção (-1 p/ SAIR): ");
+        int codigo = leitor.nextInt();
+        return codigo;
     }
 }

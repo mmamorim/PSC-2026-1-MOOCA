2474ac4dd005c167d7ce84877a2e846f43d7c30e
Marcelo <marc.m.amorim@gmail.com>
2026-04-28 09:45:10 -0300

    v1

---
+++ aula09/Forca.java
@@ -0,0 +1,13 @@
+public class Forca {
+    
+    public static void main(String[] args) {
+        
+        Screen.clear();
+
+        System.out.println("+============================+");
+        System.out.println("| Jogo da Forca - versão 1.0 |");
+        System.out.println("+============================+");
+
+
+    }
+}
